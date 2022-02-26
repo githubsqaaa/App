f5f038cc2d7d9eda7f441691988c7146be8e6742
Qi Sun <95346697+githubsqaaa@users.noreply.github.com>
2022-02-26 15:27:02 +0800

    Add files via upload

---
+++ lib/screens/complete_profile/components/body.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/constants.dart';
+import 'package:shop_app/size_config.dart';
+
+import 'complete_profile_form.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding:
+              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: SizeConfig.screenHeight * 0.03),
+                Text("完善个人信息", style: headingStyle),
+                Text(
+                  "完善信息或  \n通过其他社交媒体登录",
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: SizeConfig.screenHeight * 0.06),
+                CompleteProfileForm(),
+                SizedBox(height: getProportionateScreenHeight(30)),
+                Text(
+                  "继续则默认同意我们的条款和内容",
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.caption,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
